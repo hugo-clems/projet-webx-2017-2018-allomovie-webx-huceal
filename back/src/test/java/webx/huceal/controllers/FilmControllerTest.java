9efffd3c676133bc93546b30410e1ee715a44a54
hugo.clems <hugoc.pro@gmail.com>
2018-04-08 17:05:28 +0200

    #1 Tests unitaire FilmController

---
+++ back/src/test/java/webx/huceal/controllers/FilmControllerTest.java
@@ -0,0 +1,133 @@
+package webx.huceal.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import webx.huceal.ErrorMessage;
+import webx.huceal.domains.Film;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FilmControllerTest {
+
+	private FilmController filmController = new FilmController();
+	
+	private Film starWarsV;
+	private List<Film> allStarWars;
+	private List<Film> allStarWarsIn2005;
+	private Response responseSW5;
+	private Response responseAllSW;
+	private Response responseAllSWIn2005;
+	private Response badId;
+	private Response badTitle;
+	private Response badYear;
+
+	@Before
+	public void setUp() throws Exception {
+		starWarsV = new Film("tt0080684", "Star Wars: Episode V - The Empire Strikes Back",
+				"1980", "124 min", "Action, Adventure, Fantasy", "Twentieth Century Fox", "Irvin Kershner",
+				"Leigh Brackett (screenplay by), Lawrence Kasdan (screenplay by), George Lucas (story by)",
+				"Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams", "After the rebels are overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda. His friends accept shelter from a questionable ally as Darth Vader hunts them in a plan to capture Luke.",
+				"https://images-na.ssl-images-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg");
+
+		allStarWars = new ArrayList<>();
+		allStarWars.add(new Film("tt0076759", "Star Wars: Episode IV - A New Hope", "1977", "https://ia.media-imdb.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"));
+		allStarWars.add(new Film("tt0080684", "Star Wars: Episode V - The Empire Strikes Back", "1980", "https://images-na.ssl-images-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"));
+		allStarWars.add(new Film("tt0086190", "Star Wars: Episode VI - Return of the Jedi", "1983", "https://images-na.ssl-images-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"));
+		allStarWars.add(new Film("tt2488496", "Star Wars: The Force Awakens", "2015", "https://ia.media-imdb.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"));
+		allStarWars.add(new Film("tt0120915", "Star Wars: Episode I - The Phantom Menace", "1999", "https://ia.media-imdb.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"));
+		allStarWars.add(new Film("tt0121766", "Star Wars: Episode III - Revenge of the Sith", "2005", "https://images-na.ssl-images-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"));
+		allStarWars.add(new Film("tt0121765", "Star Wars: Episode II - Attack of the Clones", "2002", "https://ia.media-imdb.com/images/M/MV5BOWNkZmVjODAtNTFlYy00NTQwLWJhY2UtMmFmZTkyOWJmZjZiL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg"));
+		allStarWars.add(new Film("tt3748528", "Rogue One: A Star Wars Story", "2016", "https://ia.media-imdb.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg"));
+		allStarWars.add(new Film("tt2527336", "Star Wars: The Last Jedi", "2017", "https://ia.media-imdb.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"));
+		allStarWars.add(new Film("tt1185834", "Star Wars: The Clone Wars", "2008", "https://ia.media-imdb.com/images/M/MV5BMTI1MDIwMTczOV5BMl5BanBnXkFtZTcwNTI4MDE3MQ@@._V1_SX300.jpg"));
+
+		allStarWarsIn2005 = new ArrayList<>();
+		allStarWarsIn2005.add(new Film("tt0121766", "Star Wars: Episode III - Revenge of the Sith", "2005", "https://images-na.ssl-images-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"));
+		allStarWarsIn2005.add(new Film("tt0457489", "Star Wars: Revelations", "2005", "https://ia.media-imdb.com/images/M/MV5BMTYxNzU0NTc3MF5BMl5BanBnXkFtZTcwNTM1MzEzMQ@@._V1_SX300.jpg"));
+		allStarWarsIn2005.add(new Film("tt0459164", "Star Wars: A Musical Journey", "2005", "https://images-na.ssl-images-amazon.com/images/M/MV5BYWYxODQwMDQtMTY4ZS00ZWNhLWFmNDktNGQ1YWE1ODQzYmI1XkEyXkFqcGdeQXVyMTMxMjkxOTU@._V1_SX300.jpg"));
+		allStarWarsIn2005.add(new Film("tt2008565", "Lego Star Wars: Revenge of the Brick", "2005", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQxMzdiYTgtNmVkNC00ZGM3LWFkZmItZjNiNDNkNzdmNmFmXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_SX300.jpg"));
+		allStarWarsIn2005.add(new Film("tt0462554", "'Star Wars': Feel the Force", "2005", "N/A"));
+		allStarWarsIn2005.add(new Film("tt1970025", "Star Wars Heroes & Villains", "2005", "https://images-na.ssl-images-amazon.com/images/M/MV5BNGQ5Zjk1ZjEtYTY4YS00N2ZkLWJiZDAtNTgxNTgwMzg5MmUyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNzA4NDk0NjQ@._V1_SX300.jpg"));
+		allStarWarsIn2005.add(new Film("tt0790824", "World Premiere 'Star Wars III: Revenge of the Sith'", "2005", "N/A"));
+		allStarWarsIn2005.add(new Film("tt0469106", "How to Stand in Line for Star Wars", "2005", "http://ia.media-imdb.com/images/M/MV5BMTMyMTQwNjEzOV5BMl5BanBnXkFtZTcwODg4MDI1MQ@@._V1_SX300.jpg"));
+		allStarWarsIn2005.add(new Film("tt4273912", "Star Wars Episode III: Becoming Obi-Wan", "2005", "https://ia.media-imdb.com/images/M/MV5BNzExYzA4ODctMDA0Yy00M2RhLTgyNzQtM2MyMzlhNmEzZTYyXkEyXkFqcGdeQXVyMzYyMzU2OA@@._V1_SX300.jpg"));
+		allStarWarsIn2005.add(new Film("tt4528700", "Star Wars Epizod III - Imladris", "2005", "N/A"));
+
+		responseSW5 = Response.status(Response.Status.OK)
+				.type(MediaType.APPLICATION_JSON)
+				.entity(starWarsV).build();
+
+		responseAllSW = Response.status(Response.Status.OK)
+				.type(MediaType.APPLICATION_JSON)
+				.entity(allStarWars).build();
+
+		responseAllSWIn2005 = Response.status(Response.Status.OK)
+				.type(MediaType.APPLICATION_JSON)
+				.entity(allStarWarsIn2005).build();
+
+		badId = Response.status(Response.Status.BAD_REQUEST)
+				.type(MediaType.APPLICATION_JSON)
+				.entity(new ErrorMessage("Identifiant invalide !")).build();
+
+		badTitle = Response.status(Response.Status.BAD_REQUEST)
+				.type(MediaType.APPLICATION_JSON)
+				.entity(new ErrorMessage("Titre invalide !")).build();
+
+		badYear = Response.status(Response.Status.BAD_REQUEST)
+				.type(MediaType.APPLICATION_JSON)
+				.entity(new ErrorMessage("Année invalide !")).build();
+	}
+
+	@Test
+	public void findById() throws Exception {
+		Response response = filmController.findById("tt0080684");
+		assertEquals(response.getStatus(), responseSW5.getStatus());
+		assertEquals(response.getHeaders(), responseSW5.getHeaders());
+		assertEquals(response.getEntity(), responseSW5.getEntity());
+	}
+
+	@Test
+	public void findFilms() throws Exception {
+		// Rechercher par titre
+		Response response = filmController.findFilms("star wars", "");
+		assertEquals(response.getStatus(), responseAllSW.getStatus());
+		assertEquals(response.getHeaders(), responseAllSW.getHeaders());
+		assertEquals(response.getEntity(), responseAllSW.getEntity());
+
+		// Recherche par titre et année
+		response = filmController.findFilms("star+wars", "2005");
+		assertEquals(response.getStatus(), responseAllSWIn2005.getStatus());
+		assertEquals(response.getHeaders(), responseAllSWIn2005.getHeaders());
+		assertEquals(response.getEntity(), responseAllSWIn2005.getEntity());
+	}
+
+	@Test
+	public void findByIdWithBadId() throws Exception {
+		Response response = filmController.findById("tt008068");
+		assertEquals(response.getStatus(), badId.getStatus());
+		assertEquals(response.getHeaders(), badId.getHeaders());
+		assertEquals(response.getEntity(), badId.getEntity());
+	}
+
+	@Test
+	public void findFilmsWithBadTitle() throws Exception {
+		Response response = filmController.findFilms("s", "");
+		assertEquals(response.getStatus(), badTitle.getStatus());
+		assertEquals(response.getHeaders(), badTitle.getHeaders());
+		assertEquals(response.getEntity(), badTitle.getEntity());
+	}
+
+	@Test
+	public void findFilmsWithBadYear() throws Exception {
+		Response response = filmController.findFilms("star+wars", "20x5");
+		assertEquals(response.getStatus(), badYear.getStatus());
+		assertEquals(response.getHeaders(), badYear.getHeaders());
+		assertEquals(response.getEntity(), badYear.getEntity());
+	}
+
+}
