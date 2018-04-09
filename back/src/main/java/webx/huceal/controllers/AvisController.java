e6b26498dc6ea9074ac9cbce23469dfd3fdc1955
celinemorin <celine.morin.42@gmail.com>
2018-04-09 14:55:47 +0200

    debut de javadoc avis - en cours

--- back/src/main/java/webx/huceal/controllers/AvisController.java
+++ back/src/main/java/webx/huceal/controllers/AvisController.java
@@ -8,35 +8,52 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/avis")
+@Produces({ MediaType.APPLICATION_JSON })
 public class AvisController {
 
 	private AvisService service = new AvisService();
 
+	/**
+	 * Récupère tous les avis par film.
+	 * @param id identifiant du film dont on veut les avis
+	 * @return Response Json
+	 */
 	@GET
 	@Path("/film/{id}")
-	@Produces({ MediaType.APPLICATION_JSON })
 	public Response findAllAvisByFilmID(@PathParam("id") String id) {
 		return service.findAllAvisByFilmID(id);
 	}
 
+	/**
+	 * Récupère un avis grâce à son id.
+	 * @param id identifiant de l'avis demandé
+	 * @return Response Json
+	 */
 	@GET
 	@Path("/{id}")
-	@Produces({ MediaType.APPLICATION_JSON })
 	public Response findAvisByID(@PathParam("id") long id) {
 		return service.findAvisByID(id);
 	}
 
+	/**
+	 * Ajoute un avis à un film donné (note et/ou commentaire).
+	 * @param avis objet de type Avis qui doit contenir un id de film, une note et un commentaire
+	 * @return Response Json avec la location de la ressource dans le header si créée
+	 */
 	@POST
 	@Path("/add")
-	@Produces({ MediaType.APPLICATION_JSON })
 	@Consumes({ MediaType.APPLICATION_JSON })
 	public Response addAvis(Avis avis) {
 		return service.addAvis(avis.getFilmID(), avis.getNote(), avis.getCommentaire());
 	}
 
+	/**
+	 * Supprime un ou des avis par mot-clé dans les commentaires.
+	 * @param key le mot-clé
+	 * @return Response Json
+	 */
 	@DELETE
 	@Path("/delete")
-	@Produces({ MediaType.APPLICATION_JSON })
 	public Response deleteAvisByKey(@QueryParam("key") String key) {
 		return service.deleteAvisByKey(key);
 	}
--- back/src/main/java/webx/huceal/domains/Avis.java
+++ back/src/main/java/webx/huceal/domains/Avis.java
@@ -2,21 +2,50 @@ package webx.huceal.domains;
 
 public class Avis {
 
+    /**
+     * ID de l'avis dans la base de données.
+     */
     private long id;
+    /**
+     * ID du film auquel l'avis est lié.
+     */
     private String filmID;
+    /**
+     * Note de 0 à 5.
+     * Note = -1 si aucune note n'a été entrée
+     */
     private int note;
+    /**
+     * Commentaire (longueur max 500 caractères).
+     */
     private String commentaire;
 
+    /**
+     * Constructeur vide d'un avis.
+     */
     public Avis() {
         super();
     }
 
+    /**
+     * Constructeur d'un avis que l'on va ajouter à la base de données (sans id donc).
+     * @param filmID l'id du film auquel l'avis est lié (String)
+     * @param note la note donnée (int)
+     * @param commentaire le commentaire donné (String)
+     */
     public Avis(String filmID, int note, String commentaire) {
         this.filmID = filmID;
         this.note = note;
         this.commentaire = commentaire;
     }
 
+    /**
+     * Constructeur d'un avis déjà ajouté à la base de données.
+     * @param id l'id de l'avis dans la base de données
+     * @param filmID l'id du film auquel l'avis est lié (String)
+     * @param note la note donnée (int)
+     * @param commentaire le commentaire donné (String)
+     */
     public Avis(long id, String filmID, int note, String commentaire) {
         this.id = id;
         this.filmID = filmID;
