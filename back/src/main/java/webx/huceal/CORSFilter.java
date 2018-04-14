250028a423f5e938fcc31489cc168bf6b97bc79d
celinemorin <celine.morin.42@gmail.com>
2018-04-14 17:32:33 +0200

    regex pour filmID ok

--- back/src/main/java/webx/huceal/CORSFilter.java
+++ back/src/main/java/webx/huceal/CORSFilter.java
@@ -22,7 +22,7 @@ public class CORSFilter implements ContainerResponseFilter {
     public final void filter(ContainerRequestContext request, ContainerResponseContext response)
             throws IOException {
         response.getHeaders().add("Access-Control-Allow-Origin", "*");
-        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept");
         response.getHeaders().add("Access-Control-Allow-Credentials", "true");
         response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE");
     }
